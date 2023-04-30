046019e6a848c08d27c0688c9d37ec96e62a7e18
Warner Bell <warner.bell21@gmail.com>
2023-04-29 19:21:21 -0500

    update website

--- website/prod/assets/js/main.js
+++ website/prod/assets/js/main.js
@@ -32,3 +32,50 @@
 		});
 
 })(jQuery);
+
+ function blink(elem, times, speed) {
+    if (times > 0 || times < 0) {
+        if ($(elem).hasClass("blink")) 
+            $(elem).removeClass("blink");
+        else
+            $(elem).addClass("blink");
+    }
+
+    clearTimeout(function () {
+        blink(elem, times, speed);
+    });
+
+    if (times > 0 || times < 0) {
+        setTimeout(function () {
+            blink(elem, times, speed);
+        }, speed);
+        times -= .5;
+    }
+}
+
+$(document).ready(function () {
+  blink(".front-light", -1, 900);
+  
+    blink(".engage-4", -1, 650);//far right
+    blink(".engage-5", -1, 650);//far left
+blink(".engage-2", -1, 700); //center left
+  blink(".engage-3", -1, 700); //center right
+  
+  blink(".se-1", -1, 675);
+      blink(".se-2", -1,675);// right right
+   blink(".se-3", -1, 675);//right left
+   blink(".se-4", -1, 675); //left left
+
+});
+
+
+
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
