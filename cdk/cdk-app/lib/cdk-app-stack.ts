d5c376ff08c96e021956ccb7c2b6868209271a2f
Warner Bell <warner.bell21@gmail.com>
2023-05-07 16:49:30 +0000

    cdk lab updates

--- cdk/cdk-app/lib/cdk-app-stack.ts
+++ cdk/cdk-app/lib/cdk-app-stack.ts
@@ -1,15 +1,25 @@
 import * as cdk from 'aws-cdk-lib';
-import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import { HitCounter } from './hitcounter';
 
 export class CdkAppStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-        // defines an AWS Lambda resource
-        const hello = new lambda.Function(this, 'HelloHandler', {
-          runtime: lambda.Runtime.NODEJS_16_X,    // execution environment
-          code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
-          handler: 'hello.handler'                // file is "hello", function is "handler"
-        });
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler'
+    });
+
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
+    });
+
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: helloWithCounter.handler
+    });
   }
 }
