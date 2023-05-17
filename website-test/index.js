c528a63c40116c9c626bb3bc3a43fe3475e6e9d2
Warner Bell <warner.bell21@gmail.com>
2023-05-17 02:14:24 +0000

    pathing updates

--- website-test/index.js
+++ website-test/index.js
@@ -1,12 +1,15 @@
 <div class="counter-number nav-item nav-link text-white-50 font-os fontsize-16 active">Couldn't read views</div>
 
-// site-views Code
+// site-visits js
 const counter = document.querySelector(".counter-number");
+
 async function updateCounter() {
-    let response = await fetch(
-        "Lambda URL"
-    );
-    let data = await response.json();
-    counter.innerHTML = `👀 Views: ${data}`;
+  try {
+    const response = await fetch("https://d6i2pee77rzpce3yb6ly7moygq0vckfh.lambda-url.us-east-1.on.aws/");
+    const data = await response.json();
+    counter.innerHTML = `<b>Visits</b> = ${data} ✌🏽`;
+  } catch (error) {
+    console.error(error);
+  }
 }
 updateCounter();
