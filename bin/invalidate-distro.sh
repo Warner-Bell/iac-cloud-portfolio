35a6da4fa583d2c41b2db7d2d51f52faff7d9a42
Warner Bell <warner.bell21@gmail.com>
2024-08-02 15:07:45 -0500

    update index test

--- bin/invalidate-distro.sh
+++ bin/invalidate-distro.sh
@@ -1,3 +1,7 @@
+aws cloudfront create-invalidation --distribution-id E2WF56K88USHWA --paths "/*"
+
+sleep 30
+
 aws cloudfront create-invalidation \
     --distribution-id E2WF56K88USHWA \
     --paths "/*"
